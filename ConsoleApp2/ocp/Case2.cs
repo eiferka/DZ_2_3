ffa44b6a9aada292fb57a69c6dfea24204b0ebb0
eifer <150547158+eiferka@users.noreply.github.com>
2026-04-05 20:36:17 +0300

    Case2.cs
    проблемы были - в AreaCalculator.Calculate() площадь считалась через if и проверку типа объекта, из-за этого при добавлении новой фигуры пришлось бы менять уже существующий метод, а это нарушение open closed principle, потому что класс должен быть открыт для расширения, но закрыт для изменения
    ещё Calculate(object shape) работал с типом object, из-за этого код был менее понятным и менее безопасным

--- ConsoleApp2/ocp/Case2.cs
+++ ConsoleApp2/ocp/Case2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,103 +8,53 @@ namespace ConsoleApp2.ocp
 {
     class Case2
     {
-        class Circle
+        abstract class Figure
+        {
+            public abstract double GetArea();
+        }
+
+        class Circle : Figure
         {
             public double Radius;
+
             public Circle(double radius)
             {
                 Radius = radius;
             }
-            public double GetArea()
+
+            public override double GetArea()
             {
                 return Math.PI * Radius * Radius;
             }
         }
-        class Rectangle
+
+        class Rectangle : Figure
         {
             public double Width;
             public double Height;
+
             public Rectangle(double width, double height)
             {
                 Width = width;
                 Height = height;
             }
-            public double GetArea()
+
+            public override double GetArea()
             {
                 return Width * Height;
             }
         }
-        class AreaCalculator
-        {
-            public double Calculate(object shape)
-            {
-                if (shape is Circle)
-                {
-                    return ((Circle)shape).GetArea();
-                }
-                else if (shape is Rectangle)
-                {
-                    return ((Rectangle)shape).GetArea();
-                }
-                return 0;
-            }
-        }
+
         class Program
         {
             static void Main()
             {
-                Circle c = new Circle(5);
-                Rectangle r = new Rectangle(4, 6);
-                AreaCalculator calculator = new AreaCalculator();
-                Console.WriteLine("Circle area: " + calculator.Calculate(c));
-                Console.WriteLine("Rectangle area: " + calculator.Calculate(r));
+                Figure c = new Circle(5);
+                Figure r = new Rectangle(4, 6);
+
+                Console.WriteLine("Circle area: " + c.GetArea());
+                Console.WriteLine("Rectangle area: " + r.GetArea());
             }
         }
-
-
-        //class Figure
-        //{
-        //    public virtual double GetArea()
-        //    {
-        //        return 0;
-        //    }
-        //}
-
-        //class Circle : Figure
-        //{
-        //    public double Radius { get; set; }
-        //    public Circle(double radius)
-        //    {
-        //        Radius = radius;
-        //    }
-        //    public override double GetArea()
-        //    {
-        //        return Math.PI * Radius * Radius;
-        //    }
-        //}
-        //class Rectangle : Figure
-        //{
-        //    public double Width { get; set; }
-        //    public double Height { get; set; }
-        //    public Rectangle(double width, double height)
-        //    {
-        //        Width = width;
-        //        Height = height;
-        //    }
-        //    public override double GetArea()
-        //    {
-        //        return Width * Height;
-        //    }
-        //}
-        //class Application
-        //{
-        //    static void Main()
-        //    {
-        //        var c = new Circle(5);
-        //        var r = new Rectangle(4, 6);
-        //        Console.WriteLine("Circle area: " + c.GetArea());
-        //        Console.WriteLine("Rectangle area: " + r.GetArea());
-        //    }
-        //}
     }
 }
